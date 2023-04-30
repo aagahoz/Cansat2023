bb64b70055910bcc9ad71e7b9b9cd2d71c092dfc
Musab Elibol <116749792+M-Elibol@users.noreply.github.com>
2023-04-30 12:54:28 +0300

    Add files via upload

---
+++ Basic Operations/GPS_READ_NEOM8N_Libraryless/BNO_055/BNO_055_DateRead.ino
@@ -0,0 +1,29 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
+
+void setup(void)
+{
+  Serial.begin(9600);
+  if(!bno.begin())
+  {
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+  delay(1000);  
+}
+
+void loop(void)
+{
+  sensors_event_t event;
+  bno.getEvent(&event);
+  Serial.print("X: ");
+  Serial.print(event.orientation.x, 4);
+  Serial.print("\tY: ");
+  Serial.print(event.orientation.y, 4);
+  Serial.println("");
+  delay(1000);
+}
