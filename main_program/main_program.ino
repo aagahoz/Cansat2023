541a9399221babea2bbd1564284c52648bbee569
Musab Elibol <116749792+M-Elibol@users.noreply.github.com>
2023-05-06 22:50:34 +0300

    bno055 bmp280 rtc

--- main_program/main_program.ino
+++ main_program/main_program.ino
@@ -1,90 +1,144 @@
 #include <IntervalTimer.h>
 #include <SoftwareSerial.h>
+#include <Wire.h>               // BMP280
+#include <Adafruit_BMP280.h>    // BMP280
+#include <RTClib.h>             // RTC
+#include <Adafruit_Sensor.h>    // BNO055
+#include <Adafruit_BNO055.h>    // BNO055
+#include <utility/imumaths.h>   // BNO055
+
 
 // Sensors Variables Declaration
 int TEAM_ID = 1007;
-int PACKETCOUNT, PRESSURE ;
+int PACKETCOUNT, PRESSURE;
 String MODE, STATE, HS_DEPLOYED, PC_DEPLOYED, MAST_RAISED, CMD_ECHO;
 float ALTITUDE, TEMPERATURE, VOLTAGE, TILT_X, TILT_Y;
 char MISSION_TIME[32];
 char incomingChar;
 const int Xbee_rx = 7;
 const int Xbee_tx = 8;
- // EKSTRALAR
+// EKSTRALAR
 String DATA_XBEE_COMING = "";
 String DATA_INCOMING_COMMANDS = "";
- // GPS
-  typedef struct gpgga
-    {
-      char SENTENCEIDENTIFIER[7];   //1
-      float GPS_TIME;           //2
-      double GPS_LATITUDE;        //3
-      char LATUTE_DIR;       //4
-      double GPS_LONGITUDE;        //5
-      char LONGTITUDE_DIR;       //6
-      char FIX_QUALITY;       //7
-      int GPS_SATS;     //8
-      float HORIZANTAL_DILUTION;    //9
-      double GPS_ALTITUDE;        //10
-    }GPGGA;
+// GPS
+typedef struct gpgga
+{
+    char SENTENCEIDENTIFIER[7]; // 1
+    float GPS_TIME;             // 2
+    double GPS_LATITUDE;        // 3
+    char LATUTE_DIR;            // 4
+    double GPS_LONGITUDE;       // 5
+    char LONGTITUDE_DIR;        // 6
+    char FIX_QUALITY;           // 7
+    int GPS_SATS;               // 8
+    float HORIZANTAL_DILUTION;  // 9
+    double GPS_ALTITUDE;        // 10
+} GPGGA;
 GPGGA gpsDatas;
-
 
 IntervalTimer GpsTimer, I2CTimer, SendDataTimer, Timer4;
 SoftwareSerial xbee(Xbee_rx, Xbee_tx);
+Adafruit_BMP280 bmp;
+RTC_DS3231 rtc;
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
 
 
-void setup() {
-  GpsTimer.begin(GpsTimer_ISR             , 1000000); 
-  I2CTimer.begin(I2CTimer_ISR             , 1000000);  
-  SendDataTimer.begin(SendDataTimer_ISR   , 1000000); 
-  Timer4.begin(Timer4_ISR                 , 1000000);
+void setup()
+{
+    Wire.begin();
+    bmp.begin(0x76);
+    rtc.begin();
+    bno.begin();
+    rtc.adjust(DateTime(F(__DATE__),F(__TIME__)));
+    xbee.begin(9600);
 
-  xbee.begin(9600);
+    GpsTimer.begin(GpsTimer_ISR, 1000000);
+    I2CTimer.begin(I2CTimer_ISR, 1000000);
+    SendDataTimer.begin(SendDataTimer_ISR, 1000000);
+    Timer4.begin(Timer4_ISR, 1000000);
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+//    Serial.println("Loop_Func - Main Loop is running");
 }
 
-void GpsTimer_ISR(){
-    Serial.println("1_Timer - gps");
+void GpsTimer_ISR()
+{
+    Serial.println("1_Timer - gps is running");
+}
+
+void I2CTimer_ISR()
+{
+  String DATA_ALL = String(TEAM_ID) + "," + String(MISSION_TIME) + "," + String(PACKETCOUNT) + "," + MODE + "," + STATE + "," + String(ALTITUDE) + "," + HS_DEPLOYED + "," + PC_DEPLOYED + "," + MAST_RAISED + "," + String(TEMPERATURE) + "," + String(PRESSURE) + "," + String(VOLTAGE) + "," + String(gpsDatas.GPS_TIME) + "," + String(gpsDatas.GPS_ALTITUDE) + "," + String(gpsDatas.GPS_LATITUDE) + "," + String(gpsDatas.GPS_LONGITUDE) + "," + String(gpsDatas.GPS_SATS) + "," + String(TILT_X) + "," + String(TILT_Y) + "," + CMD_ECHO;
+     xbee.println(DATA_ALL);
+    Serial.println("2_Timer - Data Sended");
+    Serial.println("2_Timer - i2c is running");
+}
+
+void SendDataTimer_ISR()
+{
+
+    Serial.println("3_Timer - send is running");
     
+    if (xbee.available())
+    {
+        String incoming_data = xbee.readStringUntil('\n');
+        Serial.println("3_Timer --- XBEE is available");
+
+        Serial.println("Gelen Ham Veri -->  " + incoming_data + "   ;  " + "Boyutu -->  " + incoming_data.length()  + "   ;  " +  "Son -2 Char -->" + incoming_data[incoming_data.length() - 1]);
+
+        if (incoming_data.length() == 12)
+        {
+            if (incoming_data[0] == '!')
+            {
+                if (incoming_data[incoming_data.length() - 1] == '+')
+                {
+                    Serial.println("Alınan Fix Data : " + incoming_data);
+                }
+            }
+        }
+    }
 
 
-}
-
-void I2CTimer_ISR(){
-  Serial.println("2_Timer - i2c");
-
-}
-
-void SendDataTimer_ISR(){
-  Serial.println("3_Timer - send");
-   String DATA_ALL = String(TEAM_ID) + "," + String(MISSION_TIME) + "," + String(PACKETCOUNT) + "," + MODE+ "," + STATE+ "," + String(ALTITUDE) + "," + HS_DEPLOYED + "," + PC_DEPLOYED + "," + MAST_RAISED + "," + String(TEMPERATURE) + "," + String(PRESSURE) + "," + String(VOLTAGE) + "," + String(gpsDatas.GPS_TIME) + "," + String(gpsDatas.GPS_ALTITUDE) + "," + String(gpsDatas.GPS_LATITUDE) + "," + String(gpsDatas.GPS_LONGITUDE) + "," + String(gpsDatas.GPS_SATS) + "," + String(TILT_X) + "," + String(TILT_Y) + "," + CMD_ECHO;
-
-  if (xbee.available()) {
-    String incoming_data = xbee.readStringUntil('\n');
-            Serial.println("Gelen Ham Veri -->  " + incoming_data + "   ;  " + "Boyutu -->  " + incoming_data.length());
-
-
-  if (incoming_data.length() == 12) {
-      if (incoming_data[0] == '!') {
-        Serial.println("Alınan Fix Data : " + incoming_data);
-         
-         }
-     }
-}
-
-    
-
-  xbee.println(DATA_ALL);
+   
 
 }
 
-void Timer4_ISR(){
-  Serial.println("4_Timer - timer4");
+void Timer4_ISR()
+{
+  // BMP280
+   TEMPERATURE = bmp.readTemperature();
+   PRESSURE    = bmp.readPressure() / 100.0F;
+   ALTITUDE    = bmp.readAltitude(1013.25); // 1013.25 Referance pressure
+  
+  Serial.print ("TEMPERATURE : ");
+  Serial.println(TEMPERATURE);
+  Serial.print ("PRESSURE : ");
+  Serial.println(PRESSURE);
+  Serial.print ("ALTITUDE : ");
+  Serial.println(ALTITUDE);
 
+  
+  // RTC
+  DateTime now = rtc.now();
+  sprintf(MISSION_TIME, "%02d:%02d:%02d %02d/%02d/%02d", now.hour(), now.minute(), now.second(), now.day(), now.month(), now.year());
+  
+  Serial.print(F("DATE / TIME: "));
+  Serial.println(MISSION_TIME);
+  Serial.println("4_Timer - timer4 is running");
+
+
+  // BNO055
+  sensors_event_t event;
+  bno.getEvent(&event);
+  TILT_X= event.orientation.x;
+  TILT_Y= event.orientation.y;
+  
+  Serial.print("X: ");
+  Serial.print(TILT_X);
+  Serial.print("\tY: ");
+  Serial.print(TILT_Y);
+  Serial.println("");
 }
