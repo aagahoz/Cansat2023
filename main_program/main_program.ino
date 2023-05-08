84c1e2a0f50a24a4ed6e83a2ec6d4ba8d445dfec
Musab Elibol <116749792+M-Elibol@users.noreply.github.com>
2023-05-08 17:20:20 +0300

    Simülasyon Modu İçin Giriş (Bitmedi)

--- main_program/main_program.ino
+++ main_program/main_program.ino
@@ -9,38 +9,45 @@
 #include <Servo.h>
 
 // Sensors Variables Declaration
-int TEAM_ID = 1007;
-int PACKETCOUNT, PRESSURE;
-String MODE, STATE, HS_DEPLOYED, PC_DEPLOYED, MAST_RAISED, CMD_ECHO;
+int TEAM_ID        = 1007           ;
+int PACKETCOUNT    = 1              ;
+int PRESSURE       = 0              ;
+String MODE        = "F"            ; //‘F’ for flight mode and ‘S’ for simulation mode.
+String STATE       = "LAUNCH_WAIT"  ; // LAUNCH_WAIT, ASCENT, ROCKET_SEPARATION, DESCENT, HS_RELEASE, LANDED
+String HS_DEPLOYED = "N"            ; // ‘P’ indicates the Probe with heat shield is deployed, ‘N’ otherwise.
+String PC_DEPLOYED = "N"            ; // ‘C’ indicates the Probe parachute is deployed (at 200 m), ‘N’ otherwise.
+String MAST_RAISED = "N"            ; // ‘M’ indicates the flag mast has been raised after landing, ‘N’ otherwise.
+String CMD_ECHO     ;
 float ALTITUDE, TEMPERATURE, VOLTAGE, TILT_X, TILT_Y;
 char MISSION_TIME[32];
-char incomingChar;
-const int Xbee_rx = 7;
-const int Xbee_tx = 8;
-// EKSTRALAR
-String DATA_XBEE_COMING = "";
-String DATA_INCOMING_COMMANDS = "";
+
+// EXTRAS
+int TELEMETRY_STATE  = 1  ;  // 0 : SIM PRESSURE , 1 : REAL PRESSURE 
+int TELEMETRY_SWITCH = 0  ;  // 0 : OFF , 1 : ONN
+
 // GPS
 typedef struct gpgga
 {
     char SENTENCEIDENTIFIER[7]; // 1
     float GPS_TIME;             // 2
-    double GPS_LATITUDE;        // 3
+    double GPS_LATITUDE;        // 3  // resolution of 0.0001 degrees North.
     char LATUTE_DIR;            // 4
-    double GPS_LONGITUDE;       // 5
+    double GPS_LONGITUDE;       // 5  // resolution of 0.0001 degrees West.
     char LONGTITUDE_DIR;        // 6
     char FIX_QUALITY;           // 7
     int GPS_SATS;               // 8
     float HORIZANTAL_DILUTION;  // 9
     double GPS_ALTITUDE;        // 10
 } GPGGA;
+
 GPGGA gpsDatas;
 
 IntervalTimer GpsTimer, MainTimer, ComminicationTimer, SensorReadTimer;
-SoftwareSerial xbee(Xbee_rx, Xbee_tx);
+
 Adafruit_BMP280 bmp;
 RTC_DS3231 rtc;
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
+SoftwareSerial xbee     (7, 8);
 SoftwareSerial gpsSerial(0, 1); //
 
 int getCommaCount(String str);
@@ -55,10 +62,9 @@ void setup()
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
     xbee.begin(9600);
     gpsSerial.begin(9600);
-
+    ComminicationTimer.begin(ComminicationTimer_ISR, 1000000);
     GpsTimer.begin(GpsTimer_ISR, 1000000);
     MainTimer.begin(MainTimer_ISR, 1000000);
-    ComminicationTimer.begin(ComminicationTimer_ISR, 1000000);
     SensorReadTimer.begin(SensorReadTimer_ISR, 1000000);
 }
 
@@ -71,8 +77,8 @@ void GpsTimer_ISR()
 {
     Serial.println("GpsTimer_ISR - gps is running");
     if (gpsSerial.available())
-    {                                                     // GPS modülünden veri var mı kontrol et
-        String message = gpsSerial.readStringUntil('\n'); // Gelen veriyi oku
+    {                                                     
+        String message = gpsSerial.readStringUntil('\n'); 
         if (message.substring(0, 6) == "$GNGGA")
         { // Eğer GNGGA mesajıysa
 
@@ -98,8 +104,6 @@ void GpsTimer_ISR()
                 Serial.println(gpsDatas.HORIZANTAL_DILUTION);
                 Serial.print("Altitude: ");
                 Serial.println(gpsDatas.GPS_ALTITUDE);
-                Serial.println();
-                Serial.println();
             }
         }
     }
@@ -110,8 +114,13 @@ void MainTimer_ISR()
     Serial.println("MainTimer_ISR - i2c is running");
 
     String DATA_ALL = String(TEAM_ID) + "," + String(MISSION_TIME) + "," + String(PACKETCOUNT) + "," + MODE + "," + STATE + "," + String(ALTITUDE) + "," + HS_DEPLOYED + "," + PC_DEPLOYED + "," + MAST_RAISED + "," + String(TEMPERATURE) + "," + String(PRESSURE) + "," + String(VOLTAGE) + "," + String(gpsDatas.GPS_TIME) + "," + String(gpsDatas.GPS_ALTITUDE) + "," + String(gpsDatas.GPS_LATITUDE) + "," + String(gpsDatas.GPS_LONGITUDE) + "," + String(gpsDatas.GPS_SATS) + "," + String(TILT_X) + "," + String(TILT_Y) + "," + CMD_ECHO;
-    xbee.println(DATA_ALL);
-    Serial.println("2_Timer - Data Sended");
+    if(TELEMETRY_SWITCH==1)
+    {
+        xbee.println(DATA_ALL);
+        Serial.println("2_Timer - Data Sended");  
+        PACKETCOUNT++; 
+    }
+      
 }
 
 void ComminicationTimer_ISR()
@@ -135,18 +144,60 @@ void ComminicationTimer_ISR()
                 }
             }
         }
+
+        else if (incoming_data == "CMD,1007,SIM,ENABLE")
+        {
+            MODE= "S";  // Real pressure sending but when ACTIVE command comes, it will send fake datas.
+        }
+        
+        else if (incoming_data == "CMD,1007,SIM,ACTIVE")
+        {
+            TELEMETRY_STATE = 0; // Take fake datas with SIMP commands 
+        }
+
+        else if (incoming_data == "CMD,1007,SIM,DISABLE")
+        {
+            MODE= "F";; // Real pressure sending.
+            TELEMETRY_STATE = 1;
+        }
+
+        else if (incoming_data == "CMD,1007,CX,ON")
+        {
+            TELEMETRY_SWITCH = 1;
+        }
+
+        else if (incoming_data == "CMD,1007,CX,OFF")
+        {
+            TELEMETRY_SWITCH = 0;
+            PACKETCOUNT = 1;
+        }
+
+        else if (incoming_data == "CMD,1007,CAL")
+        {
+            
+        }
+
+        else if (incoming_data.substring(0, 14) == "CMD,1007,SIMP," && TELEMETRY_STATE == 0)
+        {
+            String command = incoming_data.substring(14, incoming_data.length()-1); // bu satır kontrol edilecek
+            PRESSURE = command.toInt();       // Fake pressure
+        }
+        else
+            Serial.println("CHECK ERROR");
     }
 }
 
 void SensorReadTimer_ISR()
 {
     Serial.println("SensorReadTimer_ISR - timer4 is running");
-
-    // BMP280
-    TEMPERATURE = bmp.readTemperature();
-    PRESSURE = bmp.readPressure() / 100.0F;
-    ALTITUDE = bmp.readAltitude(1013.25); // 1013.25 Referance pressure
-
+    if (TELEMETRY_STATE == 1) // REAL PRESSURE
+    {
+        // BMP280
+        TEMPERATURE = bmp.readTemperature();
+        PRESSURE = bmp.readPressure() / 100.0F;
+        ALTITUDE = bmp.readAltitude(1013.25); // 1013.25 Referance pressure
+    }
+    
     Serial.print("TEMPERATURE : ");
     Serial.println(TEMPERATURE);
     Serial.print("PRESSURE : ");
@@ -156,9 +207,9 @@ void SensorReadTimer_ISR()
 
     // RTC
     DateTime now = rtc.now();
-    sprintf(MISSION_TIME, "%02d:%02d:%02d %02d/%02d/%02d", now.hour(), now.minute(), now.second(), now.day(), now.month(), now.year());
+    sprintf(MISSION_TIME, "%02d:%02d:%02d %02d/%02d/%02d", now.hour(), now.minute(), now.second());
 
-    Serial.print(F("DATE / TIME: "));
+    Serial.print(F("MISSION TIME: "));
     Serial.println(MISSION_TIME);
 
     // BNO055
@@ -230,3 +281,5 @@ void parseGPGGA(String str, GPGGA &gpgga)
         }
     }
 }
+
+
